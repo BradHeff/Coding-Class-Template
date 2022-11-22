3216199f7477742ec1757cce8b517b3a14fe3c84
Brad Heffernan <ph3onix83@gmail.com>
2022-11-22 21:28:49 +1030

    updated with doc comments

--- GUI.py
+++ GUI.py
@@ -2,32 +2,97 @@
 # Horizon to practice / modify for learning purposes
 # Copyright (C) 2022  Brad Heffernan
 
+'''
+Importing image base64 code from base64_logo.py
+'''
+from base64_logo import imageString
+from base64_logo import logoString
 
+'''
+Set the window variables
+'''
 def Window(self):
-    self.W, self.H = 450, 450
-    self.title('Horizon Coding Class Template')
+    self.W, self.H = 520, 250 # Set the height and width of the window (variables declared in base.py init def)
+    self.title('Horizon Coding Class Template') # Sets the title of the window
+    ''''
+    Get the screen width and height into variable to calculate window start position
+    '''
     screen_width = self.winfo_screenwidth()
-    screen_height = self.winfo_screenheight()
-    center_x = int(screen_width/2 - self.W / 2)
-    center_y = int(screen_height/2 - self.H / 2)
-    self.geometry(f'{self.W}x{self.H}+{center_x}+{center_y}')
-    self.resizable(0, 0)
-    self.attributes("-fullscreen", False)
-    
+    screen_height = self.winfo_screenheight()    
+    '''
+    width of screen devide 2 = center screen
+    minus
+    width of tkinter window devide 2
+    equals
+    center of tkinter window is center of screen on X axis
+    '''
+    center_x = int(screen_width/2 - self.W / 2) # Calculate position X of the window start position (X = left | Right screen position)
+    '''
+    height of screen devide 2 = center screen
+    minus
+    height of tkinter window devide 2
+    equals
+    center of tkinter window is center of screen on Y axis
+    '''
+    center_y = int(screen_height/2 - self.H / 2) # Calculate position Y of the window start position (Y = top | bottom screen position)    
+    self.geometry(f'{self.W}x{self.H}+{center_x}+{center_y}') # set size and position of window start. (width x height + positionX + positionY)    
+    self.resizable(0, 0) # prevent window resize / maximize    
+    self.attributes("-fullscreen", False) # prevent window fullscreen
+
+'''
+Set the icon of the window
+'''
+def windowIcon(self, tk):
+    photo = tk.PhotoImage(data=logoString) # add base64 image string to Tk photoimage data variable
+    self.wm_iconphoto(False, photo) # set the window icon with photoimage data variable
+
+'''
+Add image to top of window
+'''
+def header(self, tk):
+    global header # declare header variable as global (This will save the image import of photoimage globally)
+    header = tk.PhotoImage(data=imageString) # add base64 image sting to Tk photoimage data variable
+    tk.Label(self, image = header).pack() # add the photoimage data variable to label and pack to window
+
+
+'''
+The main GUI function called from base.py init function
+'''
 def GUI_Window(self, tk):
-    Window(self)
     
+    Window(self) # Set the window parameters
+    windowIcon(self, tk) # Set the icon for the window
+    
+    header(self, tk) # Set the header logo inside the window
+    
+    ''' Set the default padding for the components'''
     padding = {
         'pady':10, 
         'padx':10
     }
     
+    '''Create a Label to show at top of the window'''
     lbl1 = tk.Label(self, text="This is a Lable :D ")
-    lbl1.pack(anchor=tk.N, fill=tk.X, **padding)
+    '''
+    anchor the label at the top (N = north), make the label component
+    fill the width of the available space
+    '''
+    lbl1.pack(anchor=tk.N, fill=tk.X, **padding) 
     
-    btn1 = tk.Button(self, text="Click Me!", command=self.btn_clicked)
-    
+    '''
+    Create a button
+    text = the string to display on the button
+    command = the function to run when button clicked. (Refer to base.py -> btn_clicked def)
+    add default padding reference **padding replaces (padx=10, pady=10)
+    '''
+    btn1 = tk.Button(self, text="Click Me!", command=self.btn_clicked)    
     btn1.pack(fill=tk.X, **padding)
     
+    '''
+    Create label to show on button click
+    anchor the label to bottom (S = south), make label component
+    fill the width of available space
+    add default padding reference **padding (padx=10, pady=10)
+    '''
     self.lbl2 = tk.Label(self, text="")
     self.lbl2.pack(anchor=tk.S, fill=tk.X, **padding)
--- base.py
+++ base.py
@@ -2,13 +2,25 @@
 # Horizon to practice / modify for learning purposes
 # Copyright (C) 2022  Brad Heffernan
 
-import tkinter as tk
-import GUI
+import tkinter as tk # tkinter, Python library for OOP programming
+import GUI # custom .py containing GUI code
 
-class Base(tk.Tk):
-    """The initializer function for the GUI"""
+class Base(tk.Tk): # set class argument as tkinter's Tk() function
+    """
+    The initializer function for the GUI
+    initializing self as the window base variable
+    """
     def __init__(self):
-        super(Base, self).__init__()
+        '''
+        Return a proxy object that delegates method calls to a parent or sibling class of type
+        This is useful for accessing inherited methods that have been overridden in a class.
+        Python3 introduced an a new hierachy to allow super to be called without empty.
+        Example: 
+        super().__init__()
+        is the same as 
+        super(classname, self).__init__()
+        '''
+        super(Base, self).__init__() # i still like to declare classname and self (makes compatible with python2 if people still use it)
         """ 
         This is a place holder to set the height and width of app in the GUI.py
         """
@@ -18,22 +30,28 @@ class Base(tk.Tk):
         """ 
         This is a switch toggle from 0 -> 1
         On Button click if switch = 0 we make switch = 1 and back again.        
-        """
-        
+        """        
         self.switch = 0
         
         GUI.GUI_Window(self, tk)
     
+    '''
+    This function is called from the command= variable
+    from button statement in GUI.py
+    '''
     def btn_clicked(self):
         if(self.switch == 0):
+            '''Set the text field of the label (this is made blank on initialization in GUI.py)'''
             self.lbl2['text'] = "You Clicked the Button :D "
-            self.switch = 1
+            self.switch = 1 # set switch value to 1 (indicates the button has been clicked)
         else:
+            '''Set the text field of the label (this is made blank on second click)'''
             self.lbl2['text'] = ""
-            self.switch = 0
+            self.switch = 0 # set switch value to 0 (indicates a second click, resetting back to default)
 
 
+'''Startup of the script'''
 if __name__ == "__main__":
-    app = Base()
-    app.mainloop()
+    app = Base() # initialize the class into a variable
+    app.mainloop() # Call the mainloop() function of Tk() (tkinter's show Gui window)
     
