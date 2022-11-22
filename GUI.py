b08d9eb4de4ec5327cb2a22835d76799c500bb61
Brad Heffernan <brad.heffernan83@outlook.com>
2022-11-22 18:26:52 +1030

    updated

--- GUI.py
+++ GUI.py
@@ -4,7 +4,7 @@
 
 
 def Window(self):
-    self.W, self.H = 850, 450
+    self.W, self.H = 450, 450
     self.title('Horizon Coding Class Template')
     screen_width = self.winfo_screenwidth()
     screen_height = self.winfo_screenheight()
@@ -14,5 +14,20 @@ def Window(self):
     self.resizable(0, 0)
     self.attributes("-fullscreen", False)
     
-def GUI_Window(self):
+def GUI_Window(self, tk):
     Window(self)
+    
+    padding = {
+        'pady':10, 
+        'padx':10
+    }
+    
+    lbl1 = tk.Label(self, text="This is a Lable :D ")
+    lbl1.pack(anchor=tk.N, fill=tk.X, **padding)
+    
+    btn1 = tk.Button(self, text="Click Me!", command=self.btn_clicked)
+    
+    btn1.pack(fill=tk.X, **padding)
+    
+    self.lbl2 = tk.Label(self, text="")
+    self.lbl2.pack(anchor=tk.S, fill=tk.X, **padding)
