c6a605a317ea853c23623dc086e92da4f0de3691
Brad Heffernan <brad.heffernan83@outlook.com>
2022-11-22 18:26:33 +1030

    updated

--- base.py
+++ base.py
@@ -9,12 +9,30 @@ class Base(tk.Tk):
     """The initializer function for the GUI"""
     def __init__(self):
         super(Base, self).__init__()
+        """ 
+        This is a place holder to set the height and width of app in the GUI.py
+        """
         self.W = 0
         self.H = 0
         
-        GUI.GUI_Window(self)
-
+        """ 
+        This is a switch toggle from 0 -> 1
+        On Button click if switch = 0 we make switch = 1 and back again.        
+        """
+        
+        self.switch = 0
+        
+        GUI.GUI_Window(self, tk)
     
+    def btn_clicked(self):
+        if(self.switch == 0):
+            self.lbl2['text'] = "You Clicked the Button :D "
+            self.switch = 1
+        else:
+            self.lbl2['text'] = ""
+            self.switch = 0
+
+
 if __name__ == "__main__":
     app = Base()
     app.mainloop()
